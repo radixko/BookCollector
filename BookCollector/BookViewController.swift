25e9d39eb02b83fbaff2f2d2256d944688533669
Radoslav <radoslavhlinka@gmail.com>
2017-11-13 16:47:13 +0100

    photos impl

--- BookCollector/BookViewController.swift
+++ BookCollector/BookViewController.swift
@@ -8,21 +8,28 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
+class BookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var bookImageView: UIImageView!
-    
-    
     @IBOutlet weak var titleTextField: UITextField!
     
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
     }
 
     @IBAction func photosButton(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
+    
     @IBAction func cameraButton(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func addBookButton(_ sender: Any) {
