5556890c7f2c0e46164e204cd451614e0452faef
Radoslav <radoslavhlinka@gmail.com>
2017-11-13 16:11:50 +0100

    add book view

---
+++ BookCollector/BookViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookViewController.swift
+//  BookCollector
+//
+//  Created by Radoslav Hlinka on 13/11/2017.
+//  Copyright © 2017 Radoslav Hlinka. All rights reserved.
+//
+
+import UIKit
+
+class BookViewController: UIViewController {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func photosButton(_ sender: Any) {
+    }
+    @IBAction func cameraButton(_ sender: Any) {
+    }
+    
+    @IBAction func addBookButton(_ sender: Any) {
+    }
+}
